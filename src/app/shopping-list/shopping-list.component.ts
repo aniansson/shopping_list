472f3c02923c9ea51ed3bde4191eae00e8ef6640
aniansson <ebba@aniansson.se>
2017-06-25 16:00:47 +0200

    Add design + new array fro ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from' ../shared/ingredients';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredients = [
+      new ingredients('Meat', '500g')
+  ]
   constructor() { }
 
   ngOnInit() {
